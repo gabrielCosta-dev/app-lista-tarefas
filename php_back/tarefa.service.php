04eff699e036c4d2ec062174df54d8aafdd4824e
Gabriel Lima <lima20gabriel@gmail.com>
2024-08-15 15:08:42 -0300

    Create tarefa.service.php

---
+++ php_back/tarefa.service.php
@@ -0,0 +1,21 @@
+<?php 
+
+    class TarefaService extends Tarefa {
+           public function inserir(){//create
+
+           };
+           
+           public function recuperar(){//read
+
+           };
+
+           public function atualizar(){//update
+
+           };
+
+           public function remover(){//delete
+
+           };
+    }
+
+?>
