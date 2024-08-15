f6aa635eb3ed2072c4551f4e22c6fe4b342773aa
Gabriel Lima <lima20gabriel@gmail.com>
2024-08-15 15:03:05 -0300

    Create tarefa.model.php

---
+++ php_back/tarefa.model.php
@@ -0,0 +1,19 @@
+<?php 
+
+    class Tarefa {
+        private number $id;
+        private boolean $id_status;
+        private string $tarefa;
+        private string $data_cadastro;
+
+        public function __get($atributo){
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+    }
+    
+
+?>
