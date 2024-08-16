e593cd9d9bd871d13a952c86e15f9bdc419c3971
Gabriel Lima <lima20gabriel@gmail.com>
2024-08-16 13:47:08 -0300

    updates

--- php_back/tarefa.controller.php
+++ php_back/tarefa.controller.php
@@ -1,7 +1,23 @@
 <?php 
 
+    require '../_App-Lista-Tarefas/tarefa.model.php';
+    require '../_App-Lista-Tarefas/tarefa.service.php';
+    require '../_App-Lista-Tarefas/conexao.php';
+
     echo '<pre>';
     print_r($_POST);
+    echo '</pre>';
+
+    $tarefa= new Tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->inserir();
+
+    echo '<pre>';
+    print_r($tarefaService);
     echo '</pre>';
 
 ?>
